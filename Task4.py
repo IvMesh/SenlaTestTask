662c553c006d767dc13ce976da406cef6d73b8dc
El-Ta <49769094+Phoenix007228322@users.noreply.github.com>
2019-09-26 13:35:36 +0300

    Create Task4.py

---
+++ Task4.py
@@ -0,0 +1,3 @@
+input_str = str(input("Введите предложение: "))
+searchWord = str(input("Введите искомое слово: "))
+print("Искомое слово появляется в предложении ",input_str.count(searchWord)," раз(а)")
