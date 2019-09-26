4cc59c6e46df03c03fe334a9ce1aed9de8ae1e1c
El-Ta <49769094+Phoenix007228322@users.noreply.github.com>
2019-09-26 14:23:23 +0300

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,3 +1,4 @@
 input_str = str(input("Введите предложение: "))
+input_str = input_str.lower()
 searchWord = str(input("Введите искомое слово: "))
-print("Искомое слово появляется в предложении ",input_str.count(searchWord)," раз(а)")
+print("Искомое слово появляется в предложении ",input_str.count(searchWord.lower())," раз(а)")
